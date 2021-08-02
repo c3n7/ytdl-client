ed1174543f419fb1282562fedadca9f623891144
Timothy Karani <kartimothy@gmail.com>
2021-08-02 12:19:55 +0300

    Added combobox

--- get_formats_ui.py
+++ get_formats_ui.py
@@ -1,6 +1,7 @@
 import json
 import dearpygui.dearpygui as dpg
 import dearpygui.themes as themes
+
 import youtube_dl
 
 DARK_IMGUI_THEME = themes.create_theme_imgui_dark()
@@ -36,6 +37,8 @@ with dpg.window(label="main", width=500, height=300) as main_win_id:
     dpg.add_text("Check file details")
     txt_url = dpg.add_input_text(label="URL")
 
+    dpg.add_spacing(count=2)
+
     with dpg.table(header_row=False, row_background=True,
                    borders_innerH=True, borders_outerH=True,
                    borders_innerV=True, borders_outerV=True):
@@ -43,20 +46,34 @@ with dpg.window(label="main", width=500, height=300) as main_win_id:
         dpg.add_table_column(width_fixed=True)
         dpg.add_table_column()
 
+        # row 1
         dpg.add_text("Title:")
         dpg.add_table_next_column()
         lbl_title = dpg.add_text("...", wrap=0.0)
 
+        # row 2
         dpg.add_table_next_column()
         dpg.add_text("Extractor:")
         dpg.add_table_next_column()
         lbl_extractor = dpg.add_text("...", wrap=0.0)
 
+        # row 3
         dpg.add_table_next_column()
         dpg.add_text("Format:")
         dpg.add_table_next_column()
         lbl_format = dpg.add_text("...", wrap=0.0)
 
+    dpg.add_spacing(count=2)
+
+    with dpg.table(header_row=True):
+        dpg.add_table_column(label="Video Format")
+        dpg.add_table_column(label="Audio Format")
+
+        dpg.add_combo(items=["A", "B", "C"], width=-1)
+        dpg.add_table_next_column()
+        dpg.add_combo(items=["A", "B", "C"], width=-1)
+
+    dpg.add_spacing(count=2)
     dpg.add_button(label="Get details", callback=get_details_callback)
 
 
